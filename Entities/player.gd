e4b193eaabe60945d43a4c95af0304ad8b32ff26
joshandersn <josh@inkbird.io>
2025-01-26 00:56:20 -0500

    pickup and drop working

--- Entities/player.gd
+++ Entities/player.gd
@@ -1,18 +1,47 @@
 extends CharacterBody3D
 
 @export var speed := 3
+@export var throw_power := 5
 
-var pickup_field: Array[res_item]
 
-func pickup(object: res_item) -> void:
-	var picked_up_item = object.scene.instantiate()
-	picked_up_item.freeze = true
-	$PickupPos.add_child(picked_up_item)
+var pickup_field: Array[Node3D]
+var picked_up_item: res_item
+var can_pickup := true
+
+func pickup(object: Node3D) -> void:
+	if picked_up_item:
+		drop_item()
+	picked_up_item = object.item_resource
+	var new_picked_up_item = object.item_resource.scene.instantiate()
+	new_picked_up_item.freeze = true
+	$PickupPos.add_child(new_picked_up_item)
+	object.queue_free()
+	can_pickup = false
+	$PickupTimer.start()
+
+func clear_pickups() -> void:
+	picked_up_item = null
+	for i in $PickupPos.get_children():
+		i.queue_free()
+
+func drop_item() -> void:
+	if picked_up_item:
+		var dropped_item = picked_up_item.scene.instantiate()
+		dropped_item.position = $PickupPos.global_position
+		dropped_item.linear_velocity = last_direction * throw_power
+		dropped_item.rotation.y = last_direction.x
+		add_sibling(dropped_item)
+		clear_pickups()
+	else:
+		push_warning("no item is held!")
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("pickup"):
-		if pickup_field.size() > 0:
+		if pickup_field.size() >= 1 and can_pickup:
 			pickup(pickup_field[0])
+		elif picked_up_item:
+			drop_item()
+		
 
 var last_direction: Vector3
 
@@ -35,18 +64,21 @@ func _physics_process(delta: float) -> void:
 
 func _on_pickup_field_body_entered(body: Node3D) -> void:
 	if "item_resource" in body:
-		pickup_field.append(body.item_resource)
+		pickup_field.append(body)
 		var modded_prompt = load("res://UI/Prompts/pickup.tres")
 		modded_prompt.message = str("Pickup ", body.item_resource.title)
 		Game.push_prompt.emit(load("res://UI/Prompts/pickup.tres"), 0)
-		print("found", pickup_field)
 	else:
 		push_warning(body, ' is not a valid item')
 
 func _on_pickup_field_body_exited(body: Node3D) -> void:
 	if "item_resource" in body:
-		pickup_field.erase(body.item_resource)
+		pickup_field.erase(body)
 		if pickup_field.size() <= 0:
 			Game.dismiss_all_prompts.emit()
 	else:
 		push_warning(body, ' is not a valid item')
+
+
+func _on_pickup_timer_timeout() -> void:
+	can_pickup = true
--- Items/work_light.gd
+++ Items/work_light.gd
@@ -7,5 +7,6 @@ func _ready() -> void:
 	item_resource = load("res://Items/WorkLight.tres").duplicate()
 
 func _process(delta: float) -> void:
-	if Engine.is_editor_hint():
-		rotate_y(0.1)
+	#if Engine.is_editor_hint():
+		#rotate_y(0.1)
+	pass
