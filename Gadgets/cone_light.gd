8cf4bf03f922a18fe7c53d82b2d2d28d010cc02e
joshandersn <josh@inkbird.io>
2025-01-28 05:47:51 -0500

    light hitbox fix

--- Gadgets/cone_light.gd
+++ Gadgets/cone_light.gd
@@ -6,15 +6,29 @@ extends Area3D
 func _ready() -> void:
 	update_item()
 	
+var near_array: Array[Node3D]
+var incenerate_array: Array[Node3D]
+
 func update_item() -> void:
 	$SpotLight3D.light_energy = 3 if active else 0
+	if active:
+		for i in near_array:
+			if is_instance_valid(i):
+				i.near_light_source = true
+
+		for i in incenerate_array:
+			if is_instance_valid(i):
+				i.incenerate()
 
 
 func _on_body_entered(body: Node3D) -> void:
-	if active and body.is_in_group("Enemy") and "near_light_source" in body:
-		body.near_light_source = true
-
+	if body.is_in_group("Enemy") and "near_light_source" in body:
+		near_array.append(body)
+		if active:
+			body.near_light_source = true
 
 func _on_death_zone_body_entered(body: Node3D) -> void:
-	if active and body.is_in_group("Enemy") and "incenerate" in body:
-		body.incenerate()
+	if body.is_in_group("Enemy") and "incenerate" in body:
+		incenerate_array.append(body)
+		if active:
+			body.incenerate()
