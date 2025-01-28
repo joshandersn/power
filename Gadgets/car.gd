2185bb77fcc7cbb30fd7937de645d4782df28fed
joshandersn <josh@inkbird.io>
2025-01-28 09:39:03 -0500

    enemies can hit car

--- Gadgets/car.gd
+++ Gadgets/car.gd
@@ -1,4 +1,10 @@
-extends Area3D
+extends RigidBody3D 
+
+@export var health := 100
+@export var lose_message: String
 
 func goblin_action() -> void:
-	print("goblin hit ", self)
+	health -= 1
+	if health <= 0:
+		Game.lose.emit()
+		Game.push_dialog.emit(lose_message)
