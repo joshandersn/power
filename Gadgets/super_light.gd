e5080ba51792c0dcc60a057a959191911cc21bca
joshandersn <josh@inkbird.io>
2025-01-30 15:33:47 -0500

    adds meshes

--- Gadgets/super_light.gd
+++ Gadgets/super_light.gd
@@ -11,8 +11,10 @@ func update_item() -> void:
 	if active:
 		$Area3D.monitoring = true
 		$light.light_energy = light_power
+		$Lightmesh.visible = true
 	else:
 		$light.light_energy = 0
+		$Lightmesh.visible = false
 	Game.check_level_objective.emit()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
